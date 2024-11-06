dedd48419cc11cf43cd74e6f97db17fe9aec254a
hhlitval <litvinhh@gmail.com>
2024-11-06 23:15:59 +0100

    Update main script, add back to top button and sticky header

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 "use strict";
-
-const mobileNav = document.querySelector(".burger");
-
-// mobileNav.addEventListener("click", () => {
-//   mobileNav.classList.toggle("burger--active");
-// });
-
-// Mobile nav button
 const burgerMenu = document.querySelector(".burger"),
   nav = document.querySelector(".header-nav"),
-  navList = document.querySelector(".header-nav__list");
+  navList = document.querySelector(".header-nav__list"),
+  currentYear = document.querySelector(".year");
 
+// Mobile navigation
 burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("open");
   nav.classList.toggle("mobile-nav");
   navList.classList.toggle("active");
-  // document.body.classList.toggle("no-scroll");
 });
+
+//Sticky header & goToTop button
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header"),
+    hero = document.querySelector(".hero"),
+    backToTop = document.querySelector(".back-to-top");
+  let triggerHeight = hero.offsetHeight - 170;
+
+  if (window.scrollY > triggerHeight) {
+    header.classList.add("header-sticky");
+    backToTop.classList.add("reveal");
+  } else {
+    header.classList.remove("header-sticky");
+    backToTop.classList.remove("reveal");
+  }
+});
+
+//Display current year in footer
+currentYear.textContent = new Date().getFullYear();
